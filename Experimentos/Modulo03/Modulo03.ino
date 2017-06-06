8eee8d9691fadeee5e7b408d37d5ae8643be3d21
Ademar <ademarneto1202@gmail.com>
2017-06-06 14:32:46 -0300

    Segundo Commit
    Atualizando.

--- Experimentos/Modulo03/Modulo03.ino
+++ Experimentos/Modulo03/Modulo03.ino
@@ -5,60 +5,24 @@ ROBOCORE ARDUINO KIT INICIANTE
 *
 *
 **
-Atividade do Modulo 3
-**
-Projeto Piano
+Módulo 4
 **
 \***************************************/
-const int ledPin1 = 13;
-const int ledPin2 = 12;
-const int ledPin3 = 11;
-const int Botao1 = 2;
-const int Botao2 = 3;
-const int Botao3 = 4;
-const int Buzzer = 10; //O buzzer está colocado no pino 10
-int EstadoBotao1 = 0;
-int EstadoBotao2 = 0;
-int EstadoBotao3 = 0;
-int Tom = 0; //Variavel para armazenar a nota musical
-void setup() {
-  pinMode(Buzzer, OUTPUT);
-  pinMode(ledPin1, OUTPUT);
-  pinMode(Botao1, INPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(Botao2, INPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(Botao3, INPUT);
+const int LM35 = 0;
+float temperatura = 0;
+int ADClido = 0;
+void setup(){
+Serial.begin(9600);
+analogReference(INTERNAL);
+//Se estiver usando Arduino Mega, use INTERNAL1V1
+//se estiver usando Arduino Leonardo, remova esta linha pois o Leonardo não aceita
+//este comando
 }
 void loop(){
-  EstadoBotao1 = digitalRead(Botao1);
-  EstadoBotao2 = digitalRead(Botao2);
-  EstadoBotao3 = digitalRead(Botao3);
-  if(EstadoBotao1 && !EstadoBotao2 && !EstadoBotao3) {
-    Tom = 100;
-    digitalWrite(ledPin1, HIGH);
-  }
-  if(EstadoBotao2 && !EstadoBotao1 && !EstadoBotao3) {
-    Tom = 200;
-    digitalWrite(ledPin2, HIGH);
-  }
-  if(EstadoBotao3 && !EstadoBotao2 && !EstadoBotao1) {
-    Tom = 500;
-    digitalWrite(ledPin3, HIGH);
-  }
-  if(Tom > 0) { //enquanto Tom for maior que zero faça o que esta descrit o baixo:
-  digitalWrite(Buzzer, HIGH);
-// Liga buzzer
-  delayMicroseconds(Tom);
-// Espera o tempo proporcional ao comprimento de onda da nota musical em milisegundos
-  digitalWrite(Buzzer, LOW);
-// Desliga buzzer
-  delayMicroseconds(Tom);
-// Espera o tempo proporcional ao comprimento de onda da nota musical em milisegundos
-  Tom = 0;
-// Reseta o Tom para zero, para sair do loop while e nao tocar o som constantemente
-  digitalWrite(ledPin1, LOW);
-  digitalWrite(ledPin2, LOW);
-  digitalWrite(ledPin3, LOW);
-  }
+ADClido = analogRead(LM35);
+temperatura = ADClido * 0.1075268817; //no Leonardo use 0.4887585532
+Serial.print("Temperatura = ");
+Serial.print(temperatura);
+Serial.println(" *C");
+delay(1000);
 }
